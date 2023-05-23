b84bbd46970ec7bd537e791bad9bd389530cbb9d
migueltc13 <migueltc13@gmail.com>
2023-05-23 02:26:36 +0100

    Create combat.c

---
+++ src/combat.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "player.h"
+#include "inventory.h"
+#include "item.h"
+
+/* void throwRock(Map *map, Player *p, int x, int y) {
+    if (p->inventory->nr_of_rocks > 0) {
+        p->inventory->nr_of_rocks--;
+        p->inventory->nr_items--;
+        // TODO add rock to map
+    }
+} */
